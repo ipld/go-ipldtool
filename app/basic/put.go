07e7e9d309777d197b6338f7fe589fdbffa709ee
Eric Myhre <hash@exultant.us>
2021-10-10 16:18:27 +0200

    begin put command.  skeletal.
    A *lot* of work already went on upstream:
    the go-ipld-prime library is introducing new storage APIs,
    and there's a whole new "dsadapter" module,
    which lets the new APIs be backed by the ancient go-datastore family.
    
    We're starting to use _some_ of that in this commit already,
    but this is also just a checkpoint commit about half way through.
    We'll have this whole situation bundled up invisibly inside
    the go-ipld-prime LinkSystem by the time we're done.
    
    Also: a bunch of comments about configuring of storage.
    Nothing final; those will turn into a real strategy shortly, one hopes.
    (I'm going to try to let real UX drive this, as we play with
    the CLI during its own development, via the testmark exec fixtures.)

--- app/basic/put.go
+++ app/basic/put.go
@@ -1,16 +1,80 @@
 package basic
 
 import (
-	"fmt"
+	"context"
 
+	flatfs "github.com/ipfs/go-ds-flatfs"
 	"github.com/urfave/cli/v2"
+
+	"github.com/ipld/go-ipld-prime/storage/dsadapter"
 )
 
 var Cmd_Put = &cli.Command{
 	Name:     "put",
 	Category: "Basic",
 	Usage:    "Put a single block of data into storage.",
-	Action: func(args *cli.Context) error {
-		return fmt.Errorf("not yet implemented")
-	},
+	Action:   Action_Put,
 }
+
+func Action_Put(args *cli.Context) error {
+	shardFn, err := flatfs.ParseShardFunc("/repo/flatfs/shard/v1/next-to-last/3")
+	if err != nil {
+		return err
+	}
+	ds, err := flatfs.CreateOrOpen("/tmp/foobar", shardFn, false)
+	if err != nil {
+		return err
+	}
+	defer ds.Close()
+
+	store := dsadapter.Adapter{
+		Wrapped: ds,
+	}
+
+	if err := store.Put(context.Background(), "WHAT", []byte("zonk")); err != nil { // shouting because that's what flatfs mandates.
+		return err
+	}
+
+	if err := store.Put(context.Background(), "QWEASDZXCPOILKJMNB", []byte("hork")); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Storage configuration: still in debate, but I think I'd like storage config to look roughly like this:
+/*
+
+type StorageConfig [ModedStorageSpec]
+
+type ModedStorageSpec union {
+	| "rw:" StorageModeReadWrite
+	| "ro:" StorageModeReadOnly
+	| "wb:" StorageModeWriteBack
+} representation stringprefix
+
+type StorageModeReadWrite StorageSpec
+type StorageModeReadOnly StorageSpec
+type StorageModeWriteBack StorageSpec
+
+type StorageSpec struct {
+	engine string
+	param string
+} representation stringjoin (":")
+
+*/
+// And so when taking a real system we know and looking at how that would be stated as a CLI flag:
+// `--storage=rw:flatfs:/path:/repo/flatfs/shard/v1/next-to-last/3`
+// (I can't say I care for half (or even two thirds) of the redundancy in that last hunk, but that's what flatfs does right now.  (Maybe we should tersen it up in our user-facing porcelain.))
+// That flag would also be allowed repeatedly.
+
+// Is this sufficiently ghastly that we should just force you to have a config file, and maybe make subcommands to help you set it up?
+//  Well, it's dang close.
+//  But I still want to be able to choose the modes per-command.  And that's easiest if it's controlled by flags or env vars.
+
+// Perhaps a mid-way approach:
+// You can have *no* config options in the CLI -- and make those a stateful part of the storage.
+// The only parameter left is the storage root path.
+// There are then also commands that can pre-establish a storage and configure it with options.  But this is optional.
+// And that's it.
+// This seems probably good.
--- go.mod
+++ go.mod
@@ -2,9 +2,14 @@ module github.com/ipld/go-ipldtool
 
 go 1.16
 
+// replace github.com/ipld/go-ipld-prime => ../go-ipld-prime
+// replace github.com/ipld/go-ipld-prime/storage/dsadapter => ../go-ipld-prime/storage/dsadapter
+
 require (
 	github.com/frankban/quicktest v1.13.1
-	github.com/ipld/go-ipld-prime v0.12.4-0.20211004141641-7acd7c0cf0ad
+	github.com/ipfs/go-ds-flatfs v0.4.5
+	github.com/ipld/go-ipld-prime v0.12.4-0.20211010135705-522500cfab8b
+	github.com/ipld/go-ipld-prime/storage/dsadapter v0.0.0-20211010135705-522500cfab8b
 	github.com/urfave/cli/v2 v2.3.0
 	github.com/warpfork/go-testmark v0.6.0
 )
